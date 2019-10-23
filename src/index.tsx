a5d4baabe902bb33996ea49b9fa1639c3c8e2813
AfterThreeYears <470326964@qq.com>
2019-10-23 16:17:08 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { useCallback } from 'react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const memoizedState: any[] = [];
+let cursor = 0;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function useState(initalState: any): [any, ((state: any) => void)] {
+  memoizedState[cursor] = memoizedState[cursor] || initalState;
+  const currentCursor = cursor;
+  function disptach(newState: any) {
+    memoizedState[currentCursor] = newState;
+    cursor = 0;
+    render();
+  }
+  return [memoizedState[cursor++], disptach];
+}
+
+function useEffect(callback: () => void, deps: any[]) {
+  if (!Array.isArray(memoizedState[cursor])) {
+    // first
+    memoizedState[cursor++] = deps;
+    callback();
+    return;
+  }
+  for (let i = 0; i < memoizedState[cursor].length; i++) {
+    const dep = memoizedState[cursor][i];
+    if (deps[i] !== dep) {
+      memoizedState[cursor++] = deps;
+      callback();
+      break;
+    }
+  }
+}
+
+function useRef<T>(initalVal: T | null = null) {
+  return {
+    current: initalVal,
+  }
+}
+
+
+const Counter = () => {
+  const ref = useRef('ref');
+
+  const domRef = React.useRef<HTMLParagraphElement>(null);
+
+  const [firstName, setFirstName] = useState('wang');
+  const [lastName, setLastName] = useState('shell');
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    ref.current = count;
+    console.log('useEffect', count, memoizedState, ref.current, domRef.current);
+  }, [count]);
+
+  function handleChangeFirstName() {
+    setFirstName('hello');
+  }
+  function handleChangeLastName() {
+    setLastName('world');
+  }
+
+  const handleChangeCount = useCallback(() => {
+    setCount(count + 1)
+  }, [count]);
+  return (
+    <div>
+      <p ref={domRef}>firstName is: {firstName}, lastName is: {lastName}, count is: {count}</p>
+      <button onClick={handleChangeFirstName}>click me change firstName</button>
+      <button onClick={handleChangeLastName}>click me change lastName</button>
+      <button onClick={handleChangeCount}>click me change count</button>
+    </div>
+  );
+}
+
+
+function render() {
+  ReactDOM.render(<Counter />, document.getElementById('root'));
+}
+
+render();
